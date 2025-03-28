a24e5e41fdfbf0a22d9fd33b6c3caa530da1d25a
tranthai189765 <tranthai189765@gmail.com>
2025-03-28 18:41:30 +0700

    Update LoginController.java

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000") 
+@CrossOrigin
 @RequestMapping("/api/auth")
 
 public class LoginController {
