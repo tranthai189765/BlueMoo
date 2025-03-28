74605bda6f9edd3a06ea699733da6d58f953d1d8
tranthai189765 <tranthai189765@gmail.com>
2025-03-28 18:41:41 +0700

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user") 
-@CrossOrigin(origins = "http://localhost:3000") 
+@CrossOrigin
 public class UserController {
     @Autowired
     private UserService userService;
